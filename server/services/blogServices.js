f914f4eba5454193a9f32c73966c7b61ce1e53cf
lateeshabedapudi <151735974+lateesha-bedapudi@users.noreply.github.com>
2026-07-03 11:56:01 +0530

    Update blogServices.js

--- server/services/blogServices.js
+++ server/services/blogServices.js
@@ -1,1 +1,49 @@
+const {ObjectId} = require('mongodb');
+const { getDb } = require('../config/db');
 
+const collection = () => getDb().collection('blogs');
+
+async function createBlog({topic, createdBy}){
+    const now = new Date();
+    const doc = {
+        topic,
+        seoTitle: '',
+        metaDescription: '',
+        slug: '',
+        outline: '',
+        draft: '',
+        status: 'draft',
+        revisionCount: 0,
+        createdBy: createdBy || null,
+        createdAt: now,
+        updatedAt: now,
+        publishedAt: null,
+    };
+    const {insertedId} = await collection().insertOne(doc);
+    const threadId = insertedId.toString();
+    await collection().updateOne({_id: insertedId}, {$set: {threadId}});
+    return { blogId: threadId, threadId };
+}
+
+async function setStatus(blogId, status) {
+    await collection().updateOne(
+        { _id: new ObjectId(blogId) },
+        { $set: { status, updatedAt: new Date() } }
+    );
+}
+
+async function getById(blogId) {
+    return collection().findOne({ _id: new ObjectId(blogId) });
+}
+
+async function listByStatus(status) {
+    const filter = status ? { status } : {};
+    return collection().find(filter).sort({updatedAt: -1}).toArray();
+}
+
+module.exports = {
+    createBlog,
+    setStatus,
+    getById,
+    listByStatus,
+};
